3a6f73d8cdba6133f07d0c9ff347a14819a80bee
Haolin Ma <127691823+haolinma1@users.noreply.github.com>
2025-02-14 11:11:48 -0500

    complete the view application, view approved application, view volunteer detail page

--- foodlink_frontend_v2/src/pages/dashboard/foodbank/index.js
+++ foodlink_frontend_v2/src/pages/dashboard/foodbank/index.js
@@ -19,7 +19,34 @@ const Foodbank_dashboard = () => {
     }
     checkToken();
   }, []);
-  return <div>Welcome to Foodbank dashboard</div>;
+
+  const handleGetApplication = () => {
+    router.push('/dashboard/foodbank/getApplication'); // Change this to your target page
+  };
+
+  const handleGetApproveApplication=()=>{
+    router.push('/dashboard/foodbank/getApproveApplication');
+  }
+
+
+  return <div>
+    
+    
+    
+    <div>Welcome to Foodbank dashboard</div>
+    <button 
+        onClick={handleGetApplication} 
+        className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+      >
+        fetch application
+      </button>
+      <button 
+        onClick={handleGetApproveApplication} 
+        className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+      >
+        fetch application
+      </button>
+    </div>;
 };
 
 export default Foodbank_dashboard;
