811f9ae80483069cfcef1f703fba574367fb94d0
Pratham Garg <prathamgarg456@gmail.com>
2025-03-11 19:22:03 -0400

    update inventory model and make it MainInventory model

--- foodlink_backend_v1/app/models/inventory.py
+++ foodlink_backend_v1/app/models/inventory.py
@@ -1,9 +1,18 @@
 from beanie import Document
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime, timezone
+from pydantic import Field
 
-class Inventory(Document):
-    foodbank_id: str
+class MainInventoryFoodItem(BaseModel):
     food_name: str
-    quantity: int
+    quantity: float
+
+
+class MainInventory(Document):
+    stock: List[MainInventoryFoodItem]
+    foodbank_id: str
+    last_updated: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     
     class Settings:
         collection = "inventory"
