58492488a9f6fd6098cc623b1b7dfe20c4587e84
Pratham Garg <prathamgarg456@gmail.com>
2025-04-15 22:45:17 -0400

    UI for event

--- foodlink_frontend_v2/src/pages/dashboard/individual/eventList.js
+++ foodlink_frontend_v2/src/pages/dashboard/individual/eventList.js
@@ -2,8 +2,8 @@ import EventList from '@/components/EventList';
 
 export default function EventListPage() {
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-5xl font-bold mb-6 text-center ">Events</h1>
+    <div className="container mx-auto p-6 my-20">
+      <h1 className="text-5xl font-bold mb-6 text-center uppercase">Events</h1>
       <EventList
         apiEndPoint={`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/foodlink/individual/events`}
       />
